9ba709f94947feb2408c89cccdacd4c72ce9633d
muhin <muhin.g.s.00@gmail.com>
2023-10-01 20:16:20 +0300

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -32,11 +32,11 @@ const links = [
       title: "Contact",
       url: "/contact",
     },
-    // {
-    //   id: 6,
-    //   title: "Dashboard",
-    //   url: "/dashboard",
-    // },
+    {
+      id: 6,
+      title: "Dashboard",
+      url: "/dashboard",
+    },
   ];
 
 
